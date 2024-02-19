b008e8491025e24eeb4c348a7d3c4e9095280cf0
t-adekunle <tayoadekunle97@gmail.com>
2024-02-19 12:20:42 +0000

    compelted exercise 2

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const { getAllTopics } = require('./controllers/topics.controller.js')
+
+app.use(express.json())
+
+app.get('/api/topics', getAllTopics)
+
+app.all('/*', (request, response, next) => {
+    response.status(404).send({msg: 'path not found'})
+})
+
+app.use((err, request, response, next) => {
+    if (err.status && err.msg){
+        response.status(err.status).send({msg: err.msg})
+    }
+    else{
+        next(err)
+    }
+})
+
+app.use((err, request, response, next) => {
+    response.status(500).send({msg: 'Internal server error'})
+})
+
+module.exports = app
