37506c9eb3206b19d8e116acdce502f95d47e38a
t-adekunle <tayoadekunle97@gmail.com>
2024-02-22 16:19:05 +0000

    removed comments

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,28 +1,23 @@
 const db = require("../db/connection.js");
-const fs = require("fs/promises");
+
 
 const selectAllArticles = (topic) => {
-  // const validTopics = ["cats", "mitch", "paper"];
   const queries = [];
   let sqlString = `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.body) AS comment_count FROM articles 
   LEFT JOIN comments ON comments.article_id=articles.article_id`;
 
   if (topic) {
-    // if (!validTopics.includes(topic)) {
-    //   return Promise.reject({ status: 400, msg: "bad request" });
-    // } else {
+  
       queries.push(topic);
       sqlString += ` WHERE topic = $1`;
-    // }
+  
   }
 
   sqlString += ` GROUP BY articles.article_id`;
 
   sqlString += ` ORDER BY created_at DESC`;
   return db.query(sqlString, queries).then(({ rows }) => {
-    // if (rows.length === 0) {
-    //   return Promise.reject({ status: 404, msg: "not found" });
-    // }
+
     return rows;
   });
 };
